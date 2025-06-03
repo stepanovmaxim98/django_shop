27116938f7d319310f1a4cd956b8ca9d633a9099
hexamind <stepanovmaxim98@yandex.ru>
2025-06-03 08:51:08 +0300

    create models

--- main/models.py
+++ main/models.py
@@ -13,4 +13,20 @@ class Category(models.Model):
         return self.name
     
 class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max)
+    slug = models.SlugField(max_length=100, unique=100)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
     
+    def __str__(self):
+        return self.name
+
+
